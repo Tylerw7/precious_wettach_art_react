fa606859800693170f6961bd1733fe44859ca201
Tylerw7 <t4wettach@gmail.com>
2025-12-12 14:03:46 -0500

    added images

--- precious_wettach_art_react/app/features/ProductCard.tsx
+++ precious_wettach_art_react/app/features/ProductCard.tsx
@@ -1,4 +1,5 @@
 
+import { Button } from "@/components/ui/button"
 import type { Product } from "Types/product"
 
 
@@ -16,14 +17,25 @@ type CardProduct = {
 // Card Component
 const Card = ({product}: CardProduct) => {
   return (
-    <div className="w-[280px] h-[400px] bg-blue-100">
+    <div className="
+      w-[300px] 
+      h-[450px] 
+      rounded-md
+      shadow-lg
+      flex
+      flex-col
+      ">
         <img
           src={product.pictureUrl}
           alt={product.name}
-          className="w-full h-[250px] object-cover" 
+          className="w-full h-[250px] object-cover rounded-t-md" 
         />
-        <h3>{product.name}</h3>
-        <p>{product.pictureUrl}</p>
+        <h3 className="p-4 font-bold">{product.name.toUpperCase()}</h3>
+        <p className="p-4">${product.price / 100}</p>
+        <div className="flex justify-between p-4 mt-auto">
+          <Button className="hover:cursor-pointer text-blue-400 text-[1.1rem]" variant="ghost">Add To Cart</Button>
+          <Button className="hover:cursor-pointer text-blue-400 text-[1.1rem]" variant="ghost">View</Button>
+        </div>
     </div>
   )
 }
