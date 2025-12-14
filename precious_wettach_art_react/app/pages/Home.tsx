fceff9b65b2b61fba02c2962ee873165c9cb5eb5
Tylerw7 <t4wettach@gmail.com>
2025-12-14 13:44:23 -0500

    added action and dispatch

--- precious_wettach_art_react/app/pages/Home.tsx
+++ precious_wettach_art_react/app/pages/Home.tsx
@@ -1,11 +1,13 @@
-import { useSelector} from 'react-redux'
+import { useSelector, useDispatch} from 'react-redux'
 import { Link } from 'react-router-dom'
 import type{ CounterState } from 'app/features/reducers/counterReducer'
+import { Button } from '@/components/ui/button'
 
 
 
 const Home = () => {
   const data = useSelector((state: CounterState) => state.data)
+  const dispatch = useDispatch();
 
 
   return (
@@ -14,6 +16,8 @@ const Home = () => {
         <Link to="/gallery" className='text-purple-500'>Gallery</Link>
         <Link to="/about" className='text-purple-500'>About</Link>
         <p>State: {data}</p>
+        <Button onClick={() => dispatch({type: 'increment'})} className='w-[150px]'>ADD</Button>
+        <Button onClick={() => dispatch({type: 'decrement'})} className='w-[150px]'>Minus</Button>
     </div>
   )
 }
