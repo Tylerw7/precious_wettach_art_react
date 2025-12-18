237a92814f584f567e3f63ea96f864284c4de821
Tylerw7 <t4wettach@gmail.com>
2025-12-18 15:04:29 -0500

    Added shadcn sooner toast component

--- precious_wettach_art_react/app/pages/about/About.tsx
+++ precious_wettach_art_react/app/pages/about/About.tsx
@@ -1,8 +1,37 @@
-import React from 'react'
+import { Button } from "@/components/ui/button"
+import { useLazyGet400ErrorQuery, useLazyGet401ErrorQuery, useLazyGet404ErrorQuery, useLazyGet500ErrorQuery, useLazyGetValidationErrorQuery } from "./errorApi"
+
 
 const About = () => {
+  const [trigger400Error] = useLazyGet400ErrorQuery();
+  const [trigger401Error] = useLazyGet401ErrorQuery();
+  const [trigger404Error] = useLazyGet404ErrorQuery();
+  const [trigger500Error] = useLazyGet500ErrorQuery();
+  const [triggerValidationError] = useLazyGetValidationErrorQuery();
+
+
   return (
-    <div className='mt-[150px]'>About</div>
+    <div className='mt-[150px] flex justify-center items-center'>
+        <div className="flex gap-4">
+
+            <Button className="w-[200px] h-[50px]" 
+            onClick={() => trigger400Error().catch(err => console.log(err))}>Test 400</Button>
+
+            <Button className="w-[200px] h-[50px]" 
+                        onClick={() => trigger401Error().catch(err => console.log(err))}>Test 401</Button>
+
+            <Button className="w-[200px] h-[50px]" 
+                        onClick={() => trigger404Error().catch(err => console.log(err))}>Test 404</Button>
+
+            <Button className="w-[200px] h-[50px]" 
+                        onClick={() => trigger500Error().catch(err => console.log(err))}>Test 500</Button>
+
+            <Button className="w-[200px] h-[50px]" 
+                        onClick={() => triggerValidationError().catch(err => console.log(err))}>Test Validation</Button>
+
+            
+        </div>
+    </div>
   )
 }
 
--- precious_wettach_art_react/app/store/store.ts
+++ precious_wettach_art_react/app/store/store.ts
@@ -3,16 +3,18 @@ import { counterSlice } from '../features/reducers/counterReducer';
 import { useDispatch, useSelector } from 'react-redux';
 import {galleryApi} from "../features/galleryApi"
 import {uiSlice} from "../features/uiSlice"
+import {errorApi} from "../pages/about/errorApi"
 
 
 const store = configureStore({
     reducer: {
         [galleryApi.reducerPath]: galleryApi.reducer,
+        [errorApi.reducerPath]: errorApi.reducer,
         counter: counterSlice.reducer,
         ui: uiSlice.reducer
     },
     middleware: (getDeaultMiddleware) =>
-        getDeaultMiddleware().concat(galleryApi.middleware)
+        getDeaultMiddleware().concat(galleryApi.middleware, errorApi.middleware)
 })
 
 export type RootState = ReturnType<typeof store.getState>
