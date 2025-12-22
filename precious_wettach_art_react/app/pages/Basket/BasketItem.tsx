a336a108715eb68b7d84da8cd68a090dfe872f91
Tylerw7 <t4wettach@gmail.com>
2025-12-22 10:52:06 -0500

    added basketItem

---
+++ precious_wettach_art_react/app/pages/Basket/BasketItem.tsx
@@ -0,0 +1,65 @@
+import type { Item } from "Types/basket"
+import { FaMinus } from "react-icons/fa";
+import { FaPlus } from "react-icons/fa";
+import { FaRegTrashCan } from "react-icons/fa6";
+
+
+type Props = {
+    item: Item
+}
+
+
+
+const BasketItem = ({ item }: Props) => {
+
+
+
+  return (
+    <div 
+        className="
+        h-[140px]
+        rounded-sm
+        flex
+        p-4
+        items-center
+        shadow-lg
+        gap-4
+        ">
+        <div className="w-[110px] h-[110px] rounded-sm">
+            <img 
+                src={item.pictureUrl} 
+                alt={item.name} 
+                className="object-cover rounded-sm"
+                />
+        </div>
+
+        <div className="flex flex-col gap-2 w-full">
+            <h3 className="font-bold text-[1.2rem]">{item.name}</h3>
+                <div className="flex gap-4">
+                    <p>${(item.price / 100).toFixed(2)} x {item.quantity}</p>
+                    <p>Total: <span className="text-blue-400">${(item.price / 100 * item.quantity)}</span></p>
+                </div>
+                <div className="flex gap-3">
+                    <div className="w-[30px] h-[30px] border border-red-400 rounded-sm flex justify-center items-center">
+                        <FaMinus color="red"/>
+                    </div>
+
+                    <div className="w-[30px] h-[30px] border border-black rounded-sm flex justify-center items-center">
+                        {item.quantity}
+                    </div>
+
+                    <div className="w-[30px] h-[30px] border border-green-400 rounded-sm flex justify-center items-center">
+                        <FaPlus color="green"/>
+                    </div>
+                </div>
+        </div>
+        <div className="h-full">
+            <div className="w-[30px] h-[30px] flex justify-center items-center">
+                <FaRegTrashCan color="red" size={20}/>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default BasketItem
--- precious_wettach_art_react/app/pages/Basket/BasketPage.tsx
+++ precious_wettach_art_react/app/pages/Basket/BasketPage.tsx
@@ -1,4 +1,4 @@
-
+import BasketItem from './BasketItem'
 import { useFetchBasketQuery } from './basketApi'
 
 
@@ -6,12 +6,24 @@ import { useFetchBasketQuery } from './basketApi'
 const BasketPage = () => {
     const {data, isLoading} = useFetchBasketQuery();
 
-    if (isLoading) return <div className='mt-[150px]'>Loading Basket...</div>
-    if (!data) return <div className='mt-[150px]'>Your basket is empty</div>
+    if (isLoading) return <div className='mt-[120px]'>Loading Basket!...</div>
+    if (!data) return <div className='mt-[120px]'>Your basket is empty</div>
 
     
   return (
-    <div className='mt-[150px]'>{data.basketId}</div>
+    <div className='mt-[120px] p-4'>
+
+      <div>
+      {data.items.map(item => (
+        <BasketItem item={item} key={item.productId} />
+      ))}
+      </div>
+
+      <div>
+
+      </div>
+      
+    </div>
   )
 }
 
